3dc92436bdc9a1e3510d6278c11558f827759ddc
Samy Pessé <samypesse@gmail.com>
2015-10-07 15:38:20 +0200

    Adapt tokenize.re for splitting correctly

--- test/re.js
+++ test/re.js
@@ -14,8 +14,8 @@ describe('tokenize.re()', function() {
     });
 
     it('should accept option to split instead of extracting', function() {
-        var extractUppercase = tokenize.re(/\ /i, { split: true });
-        var tokens = extractUppercase('hello world');
+        var extract = tokenize.re(/\ /i, { split: true });
+        var tokens = extract('hello world');
 
         tokens.should.have.lengthOf(3);
         tokens[0].value.should.equal('hello');
@@ -29,6 +29,18 @@ describe('tokenize.re()', function() {
         tokens[2].value.should.equal('world');
         tokens[2].index.should.equal(6);
         tokens[2].offset.should.equal(5);
+    });
+
+    it('should accept option to split instead of extracting (2)', function() {
+        var extract = tokenize.re(/\s/i, { split: true });
+        var tokens = extract('hello world\ntest');
+
+        tokens.should.have.lengthOf(5);
+        tokens[0].value.should.equal('hello');
+        tokens[1].value.should.equal(' ');
+        tokens[2].value.should.equal('world');
+        tokens[3].value.should.equal('\n');
+        tokens[4].value.should.equal('test');
     });
 });
 
