956029748f29117905ef97c8dcafefd450341369
Samy Pessé <samypesse@gmail.com>
2015-10-07 12:22:39 +0200

    Add unit test for tokenize.flow

--- README.md
+++ README.md
@@ -10,6 +10,59 @@ $ npm install tokenize-text
 
 ### Usage
 
+```js
+var tokenize = require('tokenize-text');
+```
 
+##### Tokenize as characters
+
+```js
+var tokens = tokenize.characters()('abc');
+
+/*
+[
+    { value: 'a', index: 0, offset: 1 },
+    { value: 'b', index: 1, offset: 1 },
+    { value: 'c', index: 2, offset: 1 }
+]
+*/
+```
+
+##### Compose tokenizers
+
+`tokenize.flow(fn1, fn2, ...)` is being used to compose multiple tokenizers.
+
+Example to extract all repeated words in sentences:
+
+```js
+var repeatedWords = tokenize.flow(
+    // Tokenize as sections
+    tokenize.sections(),
+
+    // For each sentence
+    tokenize.flow(
+        // Tokenize as words
+        tokenize.words(),
+
+        // For each sentences
+        tokenize.filter(function(word, token, prev) {
+            return (
+                prev &&
+                token.value.toLowerCase() === prev.value.toLowerCase()
+            );
+        })
+    )
+);
+
+
+var tokens = repeatedWords('This is great great. Great is an an awesome words');
+
+/*
+[
+    { value: 'great', index: 14, offset: 5 },
+    { value: 'an', index: 33, offset: 2 }
+]
+*/
+```
 
 
---
+++ test/flow.js
@@ -0,0 +1,40 @@
+var should = require('should');
+
+var tokenize = require("../lib");
+
+describe('Flow and Composition', function() {
+
+    it('ex: repeated words', function() {
+        var repeatedWords = tokenize.flow(
+            // Tokenize as sections
+            tokenize.sections(),
+
+            // For each sentence
+            tokenize.flow(
+                // Tokenize as words
+                tokenize.words(),
+
+                // For each sentences
+                tokenize.filter(function(word, token, prev) {
+                    return (
+                        prev &&
+                        token.value.toLowerCase() === prev.value.toLowerCase()
+                    );
+                })
+            )
+        );
+
+
+        var tokens = repeatedWords('This is great great. Great is an an awesome words');
+        tokens.should.have.lengthOf(2);
+
+        tokens[0].value.should.equal('great');
+        tokens[0].index.should.equal(14);
+        tokens[0].offset.should.equal(5);
+
+        tokens[1].value.should.equal('an');
+        tokens[1].index.should.equal(33);
+        tokens[1].offset.should.equal(2);
+    });
+});
+
